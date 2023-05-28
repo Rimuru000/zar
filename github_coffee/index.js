5257326d0a2f3c7941850815476f7b8cd35bc87f
Zhuzzaa <130728265+Zhuzzaa@users.noreply.github.com>
2023-05-28 16:32:38 +0600

    home page(navbar and photo)

---
+++ github_coffee/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.pageYOffset > 0) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  });
+  
