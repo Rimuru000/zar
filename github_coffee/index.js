cf31b71dd940d207c1033d63cafee75f954f179f
Zhuzzaa <130728265+Zhuzzaa@users.noreply.github.com>
2023-05-29 15:19:11 +0600

    rustem part and my part

--- github_coffee/index.js
+++ github_coffee/index.js
@@ -1,9 +1,17 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.pageYOffset > 0) {
-      navbar.classList.add('sticky');
-    } else {
-      navbar.classList.remove('sticky');
-    }
-  });
-  
+// Добавляем обработчик события для кнопки переключения навбара
+var navbarToggler = document.querySelector(".navbar-toggler");
+var navbarNav = document.querySelector("#navbarNav");
+
+navbarToggler.addEventListener("click", function () {
+  navbarNav.classList.toggle("collapsed");
+});
+
+// Добавляем обработчик события при прокрутке страницы
+window.addEventListener('scroll', function () {
+  var navbar = document.getElementById('navbar');
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+});
