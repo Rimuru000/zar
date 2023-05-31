7c198026f2329d5e25b3906c2ea7d8d27caea26a
Rimuru000 <118287074+Rimuru000@users.noreply.github.com>
2023-05-31 23:54:32 +0600

    d

--- github_coffee/reg.js
+++ github_coffee/reg.js
@@ -36,33 +36,30 @@ if (inputDate > today) {
 alert("You registered Successfully!")
 }
 
-function setCookie(c_name,value,exdays)
-{
-    var exdate=new Date();
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
-    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-    document.cookie=c_name + "=" + c_value;
-}
-
-function getCookie(c_name)
-{
-    var i,x,y,ARRcookies=document.cookie.split(";");
-    for (i=0;i<ARRcookies.length;i++)
-    {
-      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-      x=x.replace(/^\s+|\s+$/g,"");
-      if (x==c_name)
-        {
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+  }
+  
+  function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+      x = x.replace(/^\s+|\s+$/g, "");
+      if (x == c_name) {
         return unescape(y);
-        }
       }
-}
-
-var song = document.getElementsByTagName('audio')[0];
-var played = false;
-var tillPlayed = getCookie('timePlayed');
-function toggleMute() {
+    }
+  }
+  
+  var song = document.getElementsByTagName('audio')[0];
+  var played = false;
+  var tillPlayed = getCookie('timePlayed');
+  
+  function toggleMute() {
     if (song.muted) {
       song.muted = false;
       document.getElementById('muteButton').innerText = 'Mute';
@@ -72,22 +69,25 @@ function toggleMute() {
     }
   }
   
-function update()
-{
-    if(!played){
-        if(tillPlayed){
+  function update() {
+    if (!played) {
+      if (tillPlayed) {
         song.currentTime = tillPlayed;
         song.play();
         played = true;
-        }
-        else {
-                song.play();
-                played = true;
-        }
+      } else {
+        song.play();
+        played = true;
+      }
+    } else {
+      setCookie('timePlayed', song.currentTime);
     }
-
-    else {
-    setCookie('timePlayed', song.currentTime);
+  }
+  
+  document.addEventListener('keypress', function(event) {
+    if (event.key === 'f' || event.key === 'F') {
+      toggleMute();
     }
-}
-setInterval(update,1000);
+  });
+  
+  setInterval(update, 1000);
--- github_coffee/types.js
+++ github_coffee/types.js
@@ -9,32 +9,29 @@ window.addEventListener('scroll', function() {
     }
   });
 
-  function setCookie(c_name,value,exdays)
-  {
-      var exdate=new Date();
-      exdate.setDate(exdate.getDate() + exdays);
-      var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-      document.cookie=c_name + "=" + c_value;
+  function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
   }
   
-  function getCookie(c_name)
-  {
-      var i,x,y,ARRcookies=document.cookie.split(";");
-      for (i=0;i<ARRcookies.length;i++)
-      {
-        x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-        y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-        x=x.replace(/^\s+|\s+$/g,"");
-        if (x==c_name)
-          {
-          return unescape(y);
-          }
-        }
+  function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+      x = x.replace(/^\s+|\s+$/g, "");
+      if (x == c_name) {
+        return unescape(y);
+      }
+    }
   }
   
   var song = document.getElementsByTagName('audio')[0];
   var played = false;
   var tillPlayed = getCookie('timePlayed');
+  
   function toggleMute() {
     if (song.muted) {
       song.muted = false;
@@ -45,23 +42,25 @@ window.addEventListener('scroll', function() {
     }
   }
   
-  function update()
-  {
-      if(!played){
-          if(tillPlayed){
-          song.currentTime = tillPlayed;
-          song.play();
-          played = true;
-          }
-          else {
-                  song.play();
-                  played = true;
-          }
+  function update() {
+    if (!played) {
+      if (tillPlayed) {
+        song.currentTime = tillPlayed;
+        song.play();
+        played = true;
+      } else {
+        song.play();
+        played = true;
       }
-  
-      else {
+    } else {
       setCookie('timePlayed', song.currentTime);
-      }
+    }
   }
-  setInterval(update,1000);
   
+  document.addEventListener('keypress', function(event) {
+    if (event.key === 'f' || event.key === 'F') {
+      toggleMute();
+    }
+  });
+  
+  setInterval(update, 1000);
